722a785673d1c84f2ae662694242857b50bcd5b5
Dominier <118577440+TimCooks@users.noreply.github.com>
2023-11-19 03:42:42 -0800

    Base of Google Dark

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,8 +2,34 @@
 
 export default function HomePage() {
   return (
-    <div>
-      Homepage
+    <div className="selection:bg-gray-500 selection:text-[#f1f1f2]">
+      {/* Timothy Logo */}
+      <div className="w-[100%] p-5">
+        <h1 className="text-5xl font-bold text-center">
+          Timothy
+        </h1>
+      </div>
+
+      {/* Search bar */}
+      <div className="flex m-5 rounded-3xl w-[60%] h-[40px] bg-[#f1f1f2] ml-auto mr-auto">
+        {/* Search Logo */}
+
+
+        {/* Text */}
+        <p className="flex pl-5 text-[#6e7077] items-center">
+          Think, Create, Plan...
+        </p>
+
+      </div>
+
+      {/* List of Projects Icons */}
+      <div className="grid grid-cols-4 border border-white">
+        <div className="border border-blue-200">
+          Card 1
+        </div>
+      </div>
+
+
     </div>
   )
 }
