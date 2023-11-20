ea5c7f4b224d6e2af4fbe5757444af0db8d7eb83
Dominier <118577440+TimCooks@users.noreply.github.com>
2023-11-19 19:20:34 -0800

    Added svgs and logos to search bar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 
 export default function HomePage() {
@@ -5,20 +6,34 @@ export default function HomePage() {
     <div className="selection:bg-gray-500 selection:text-[#f1f1f2]">
       {/* Timothy Logo */}
       <div className="w-[100%] p-5">
-        <h1 className="text-6xl font-semibold text-center">
+        <h1 className="text-7xl font-semibold text-center drop-shadow-lg">
           Timothy
         </h1>
       </div>
 
       {/* Search bar */}
-      <div className="flex m-5 rounded-3xl w-[60%] h-[40px] bg-[#f1f1f2] ml-auto mr-auto">
+      <div className="flex m-5 rounded-3xl w-[60%] h-[40px] bg-[#f1f1f2] ml-auto mr-auto pl-3 pr-3 shadow-lg">
         {/* Search Logo */}
-
+        <Image 
+          src='/search.svg'
+          width={18}
+          height={18}
+          alt="search icon"
+        />
 
         {/* Text */}
-        <p className="flex pl-5 text-[#6e7077] items-center">
+        <p className="flex pl-2 text-[#6e7077] items-center">
           Think, Create, Plan...
         </p>
+
+        {/* Camera Logo */}
+        <Image 
+          src='/camera.svg'
+          width={18}
+          height={18}
+          alt="camera icon"
+          className="relative ml-auto"
+        />
 
       </div>
 
