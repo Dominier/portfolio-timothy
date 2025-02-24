208850917a0664cb600554ed9dd8b77bd2e6a5a2
Timothy Le <tle0279@gmail.com>
2025-02-23 21:37:49 -0800

    Web redesign

---
+++ app/(routes)/blog/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { NavigationBar } from "@/components/ui/navbar";
+
+export default function BlogPage() {
+  return (
+    <div className="">
+        <div className="text-center justify-center align-middle p-20">
+            Coming soon... (still not sure what I am making yet!)
+        </div>
+
+    </div>
+  )
+}
